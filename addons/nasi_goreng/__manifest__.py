e91bdf87d40711f91ed3c64b7288135072d00fe6
Muhammad Ruffian Azzahri <muhammadruffianazzahri@gmail.com>
2025-03-05 01:47:42 +0700

    init

---
+++ addons/nasi_goreng/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Nasi Goreng Odoo by Ruffian',
+    'version': '1.0',
+    'summary': 'Menu nasi goreng + CRUD',
+    'description': 'Module untuk mengelola berbagai jenis nasi goreng',
+    'author': 'Nama Kamu',
+    'depends': ['base'],
+    'data': [
+        'views/nasi_goreng_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
